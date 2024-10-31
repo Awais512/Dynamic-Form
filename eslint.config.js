e71da02fb7359e9e115b340e8b199c1dd56920df
awais512 <raza.awais25@gmail.com>
2024-10-31 20:22:09 +0000

    Build fix

--- eslint.config.js
+++ eslint.config.js
@@ -24,7 +24,6 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       rules: {
-        "no-unused-vars": 0,
         "@typescript-eslint/no-unused-vars": 0,
         "@typescript-eslint/no-explicit-any": "off",
       },
